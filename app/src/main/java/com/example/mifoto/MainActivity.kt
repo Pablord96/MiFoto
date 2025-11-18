c37ab47c1871dd6e9cf706f14305c7d22851373c
BUZZ-Pablo <prd483@educa.madrid.org>
2025-11-18 13:26:41 +0100

    Third commit

--- app/src/main/java/com/example/mifoto/MainActivity.kt
+++ app/src/main/java/com/example/mifoto/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,11 +20,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mifoto.ui.theme.MiFotoTheme
@@ -44,6 +49,8 @@ fun MiFoto() {
     //Declaramos una variable observable para almacenar el color de fondo
     //val colorFondo = remember {mutableStateOf(Color.Blue)}
     var colorFondo by remember { mutableStateOf(Color.Blue) }
+    var posicionTexto by remember { mutableStateOf(Offset(0f, 0f)) }
+
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -63,12 +70,32 @@ fun MiFoto() {
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .padding(16.dp)
-                .align(Alignment.Center))
+                //.align(Alignment.Center)
+                .offset{
+                    IntOffset(posicionTexto.x.toInt(),
+                        posicionTexto.y.toInt())
+                }
+                .pointerInput(Unit){
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()//evita que se activen otros eventos
+                        posicionTexto = Offset (
+                            posicionTexto.x+dragAmount.x,
+                            posicionTexto.y+dragAmount.y
+                        )
+                    }
+                }
+                /*.pointerInput(Unit){
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        posicionTexto = posicionTexto.plus(dragAmount)
+                    }
+                }*/
+        )
         Button(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(16.dp),
-            onClick = { colorFondo = colorAleatorio() }
+            onClick = { colorFondo = colorAleatorio()}
         ) {
             Text(text = "Cambiar Fondo")
         }
