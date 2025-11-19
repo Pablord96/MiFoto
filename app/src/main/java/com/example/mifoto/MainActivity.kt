db59a89c2d20cea6cb7b064bf1e34dcdc518cb1a
BUZZ-Pablo <prd483@educa.madrid.org>
2025-11-19 13:35:49 +0100

    Fourth commit

--- app/src/main/java/com/example/mifoto/MainActivity.kt
+++ app/src/main/java/com/example/mifoto/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -22,7 +23,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -50,25 +53,43 @@ fun MiFoto() {
     //val colorFondo = remember {mutableStateOf(Color.Blue)}
     var colorFondo by remember { mutableStateOf(Color.Blue) }
     var posicionTexto by remember { mutableStateOf(Offset(0f, 0f)) }
-
+    //Calculamos el ancho y el alto de la pantalla
+    var anchoPantalla by remember { mutableStateOf(0f) }
+    var altoPantalla by remember { mutableStateOf(0f) }
+    //Calculamos el ancho y el alto del texto
+    var anchoTexto by remember { mutableStateOf(0f) }
+    var altoTexto by remember { mutableStateOf(0f) }
 
     Box(modifier = Modifier
         .fillMaxSize()
         .padding(20.dp)
         //.background(colorFondo.value))
-        .background(colorFondo))
-    {
-        Image(
+        .background(colorFondo)
+        .onGloballyPositioned{coordinates ->
+            altoPantalla = coordinates.size.height.toFloat()
+            anchoPantalla = coordinates.size.width.toFloat()
+        }
+    ){
+        /*Image(
             painter = painterResource(R.drawable.philadelphia_eagles_logo_svg),
             contentDescription = "Philadelphia Eagles",
             modifier = Modifier.align(Alignment.Center).fillMaxSize()
-        )
+        )*/
+        imagenInteractiva()
         Text(
             text = "Philadelphia Eagles", color = Color.Red,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             modifier = Modifier
+                .onGloballyPositioned{coordinates ->
+                    altoTexto = coordinates.size.height.toFloat()
+                    anchoTexto = coordinates.size.width.toFloat()
+                    if(posicionTexto == Offset(0f, 0f)){
+                        posicionTexto = Offset((anchoPantalla-anchoTexto)/2,
+                            (altoPantalla-altoTexto)/2)
+                    }
+                }
                 .padding(16.dp)
                 //.align(Alignment.Center)
                 .offset{
@@ -107,6 +128,45 @@ fun colorAleatorio(): Color {
     val green = (0..255).random()
     val blue = (0..255).random()
     return Color(red, green, blue)
+}
+
+@Composable
+fun imagenInteractiva(){
+    //Necesitamos almacenar y observar la escala de la imagen
+    var escala by remember { mutableStateOf(1f) }
+    //Necesitamos la posicion de la imagen
+    var posicion by remember { mutableStateOf(Offset.Zero) }
+
+    var rotacion by remember { mutableStateOf(0f) }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit){
+            detectTransformGestures {_, desplazamiento, zoom, rotationAngle ->
+                posicion += desplazamiento // Aplicamos el desplazamiento a la posicion
+                escala *= zoom //Aplicamos el zoom a la escala
+                rotacion += rotationAngle
+            }
+        }
+        , contentAlignment = Alignment.Center)
+    {
+        Image(
+            painter = painterResource(R.drawable.philadelphia_eagles_logo_svg),
+            contentDescription = "Philadelphia Eagles",
+            modifier = Modifier
+                //graphicsLayer permite aplicar una escala y una posicion a la imagen
+                .graphicsLayer(
+                    //Desplazamiento
+                    translationX = posicion.x,
+                    translationY = posicion.y,
+                    //Limite Zoom del eje X
+                    scaleX = escala.coerceIn(0.5f, 3f),
+                    //Limite Zoom del eje Y
+                    scaleY = escala.coerceIn(0.5f, 3f),
+                    rotationZ = rotacion
+                )
+        )
+    }
 }
 
 @Preview(showBackground = true)
